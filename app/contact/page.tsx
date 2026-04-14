99deb808c90e10bc31a8ab5ec0322d438ca36077
Husniddinkhon <64948863+Husniddinkhon@users.noreply.github.com>
2026-04-14 06:17:35 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import MIZANIAWebsiteV2 from "@/components/MizaniaSite";
+import MIZANIAWebsiteV2 from "../../components/MizaniaSite";
 
 export const metadata: Metadata = {
   title: "Aloqa",
   description:
-    "MIZANIA bilan bog‘lanish, konsultatsiya so‘rash va hamkorlik бўйича murojaat yuborish sahifasi.",
+    "MIZANIA bilan bog‘lanish, konsultatsiya so‘rash va hamkorlik bo‘yicha murojaat yuborish sahifasi.",
 };
 
 export default function ContactPageRoute() {
